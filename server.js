c7946c2a7bb1f82e79fd2a6374a6ed3d228486e3
Brock T <brockthigpen@yahoo.com>
2020-01-30 19:08:41 -0500

    fixed eslint errors

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-let express = require("express");
+let express = require('express');
 
 let PORT = process.env.PORT || 8080;
 
 let app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Serve static content for the app from the 'public' directory in the application directory.
+app.use(express.static('public'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-let exphbs = require("express-handlebars");
+let exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-let routes = require("./controllers/burgerController.js");
+let routes = require('./controllers/burgerController.js');
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+// Log (server-side) when our server has started
+  console.log('Server listening on: http://localhost:' + PORT);
 });
