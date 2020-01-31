284fd63dae85efac17b0f895fbc062f8562314a3
Kacie Hatley <kaciehatley@gmail.com>
2020-01-31 10:25:30 -0500

    travis fixes

--- test/server.js
+++ test/server.js
@@ -5,17 +5,13 @@ function bookSearch() {
 
   if(document.getElementById('keyword').checked) {
     search = document.getElementById('search').value;
-  }
-  else if(document.getElementById('title').checked) {
+  } else if(document.getElementById('title').checked) {
     search = 'intitle:' + document.getElementById('search').value;
-  }
-  else if(document.getElementById('author').checked) {
+  } else if(document.getElementById('author').checked) {
     search = 'inauthor:' + document.getElementById('search').value;
-  }
-  else if(document.getElementById('isbn').checked) {
+  } else if(document.getElementById('isbn').checked) {
     search = 'isbn:' + document.getElementById('search').value;
-  }
-  else {
+  } else {
     alert('nope');
   }
 
@@ -24,15 +20,15 @@ function bookSearch() {
     dataType: 'json',
     type: 'GET'
   }).then(function(data) {
-      for (i=0;i<data.items.length; i++) {
-        results.innerHTML += '<h2>' + data.items[i].volumeInfo.title + '</h2>';
-        results.innerHTML +='<p>Author: ' + data.items[i].volumeInfo.authors[i] + '</p>';
-        results.innerHTML +='<p>Description: ' + data.items[i].volumeInfo.description + '</p>';
-        results.innerHTML +='<p>ISBN: ' + data.items[i].volumeInfo.industryIdentifiers[0].identifier + '</p>';
-        results.innerHTML +='<p>Page Count: ' + data.items[i].volumeInfo.pageCount + '</p>';
-        results.innerHTML +='<img src="' + data.items[i].volumeInfo.imageLinks.thumbnail + '">'
-      }
-    })
+    for (i=0;i<data.items.length; i++) {
+      results.innerHTML += '<h2>' + data.items[i].volumeInfo.title + '</h2>';
+      results.innerHTML +='<p>Author: ' + data.items[i].volumeInfo.authors[i] + '</p>';
+      results.innerHTML +='<p>Description: ' + data.items[i].volumeInfo.description + '</p>';
+      results.innerHTML +='<p>ISBN: ' + data.items[i].volumeInfo.industryIdentifiers[0].identifier + '</p>';
+      results.innerHTML +='<p>Page Count: ' + data.items[i].volumeInfo.pageCount + '</p>';
+      results.innerHTML +='<img src="' + data.items[i].volumeInfo.imageLinks.thumbnail + '">'
+    }
+  });
 }
 
 document.getElementById('button').addEventListener('click', bookSearch);
