2e3ff76781fec51069fe47a3fa0f66cc629f9064
Brock T <brockthigpen@yahoo.com>
2020-02-03 20:25:28 -0500

    pre pull commit

--- models/books.js
+++ models/books.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-  var Books = sequelize.define('books', {
+  var Books = sequelize.define('Books', {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
